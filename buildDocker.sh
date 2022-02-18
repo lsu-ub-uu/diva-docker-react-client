49282241aa53660778be16a22c2a0f54b0605844
belanglos <benjamin@longstream.se>
2022-02-18 15:06:47 +0100

    Added echo to show base_url.

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,6 +2,8 @@
 
 BASE_URL=${1:-'https://cora.test.diva-portal.org/diva/rest/'}
 
+echo Building diva-docker-react-client with BASE_URL $BASE_URL
+
 docker build --no-cache --pull \
     --build-arg BASE_URL=$BASE_URL \
     -t diva-docker-react-client ./docker
