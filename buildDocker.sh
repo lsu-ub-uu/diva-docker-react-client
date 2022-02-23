8a1e3466b8f71687e0efc140d5186d1b64f25bf8
belanglos <benjamin@longstream.se>
2022-02-23 15:43:17 +0100

    Added BASENAME.

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 BASE_URL=${1:-'https://cora.test.diva-portal.org/diva/rest/'}
+BASENAME=${2:-''}
 
-echo Building diva-docker-react-client with BASE_URL $BASE_URL
+echo Building diva-docker-react-client with BASE_URL $BASE_URL and BASENAME $BASENAME
 
 docker build --no-cache --pull \
     --build-arg BASE_URL=$BASE_URL \
+    --build-arg BASENAME=$BASENAME \
     -t diva-docker-react-client ./docker
