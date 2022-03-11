69ea135b56e3bec148b59df48d92505c02f1a25d
belanglos <benjamin@longstream.se>
2022-03-11 16:36:48 +0100

    Added possibility to specify version.

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,10 +2,13 @@
 
 BASE_URL=${1:-'https://cora.test.diva-portal.org/diva/rest/'}
 BASENAME=${2:-'/'}
+VERSION=${3:-'latest'}
 
-echo Building diva-docker-react-client with BASE_URL $BASE_URL and BASENAME $BASENAME
+TAG=diva-docker-react-client:$VERSION
+
+echo Building diva-docker-react-client with BASE_URL $BASE_URL, BASENAME $BASENAME and VERSION $VERSION
 
 docker build --no-cache --pull \
     --build-arg BASE_URL=$BASE_URL \
     --build-arg BASENAME=$BASENAME \
-    -t diva-docker-react-client ./docker
+    -t $VERSION ./docker
